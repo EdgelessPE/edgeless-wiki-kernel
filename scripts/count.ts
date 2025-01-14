e1c7d637b0eb6b9355cf6c6650256dadf975c68e
Cno <dongheming@bytedance.com>
2025-01-14 11:21:02 +0800

    feat: 改造计数脚本

--- scripts/count.ts
+++ scripts/count.ts
@@ -4,8 +4,7 @@ import path from "path"
 function main(){
     const textPath=path.join(__dirname,"..","docs","global","donate.md")
     const text=fs.readFileSync(textPath).toString()
-    const sp=text.split(/## [0-9]+年/)
-    const matches=sp[1].match(/￥ [\d\.]+\s*/g)
+    const matches=text.match(/￥ [\d\.]+\s*/g)
     if(matches){
         let money,sum=0
         for(let mt of matches){
